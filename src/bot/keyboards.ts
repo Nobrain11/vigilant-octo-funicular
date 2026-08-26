20e2a2390e944dd2a28ad3724513d9020bed490e
Nobrain11 <chainpulseink@gmail.com>
2026-08-26 15:52:34 +0100

    Update keyboards.ts

--- src/bot/keyboards.ts
+++ src/bot/keyboards.ts
@@ -3,7 +3,7 @@ import TelegramBot from 'node-telegram-bot-api';
 export function buyKeyboard(ca: string): TelegramBot.InlineKeyboardMarkup {
   return {
     inline_keyboard: [
-      [{ text: 'Buy 0.1 SOL', ucallback_data: `buy_${ca}_0.1` }],
+      [{ text: 'Buy 0.1 SOL', callback_data: `buy_${ca}_0.1` }],
       [{ text: 'Buy 0.5 SOL', callback_data: `buy_${ca}_0.5` }],
       [{ text: 'Custom', callback_data: `buy_${ca}_custom` }],
       [{ text: 'Cancel', callback_data: `buy_${ca}_cancel` }]
