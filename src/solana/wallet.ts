5ccc3911a709cec985f5195195e772bd1dcfd489
Nobrain11 <chainpulseink@gmail.com>
2026-08-26 05:01:17 +0100

    Create wallet.ts

---
+++ src/solana/wallet.ts
@@ -0,0 +1,20 @@
+import { Keypair } from '@solana/web3.js';
+import { encryptPrivateKey, decryptPrivateKey } from '../security/crypto.js';
+import { config } from '../config.js';
+
+export function createKeypair(): Keypair {
+  return Keypair.generate();
+}
+
+export function keypairFromPrivateKeyBase58(secret: string): Keypair {
+  return Keypair.fromSecretKey(bs58.decode(secret));
+}
+
+export function encryptKeypair(kp: Keypair): string {
+  return encryptPrivateKey(kp.secretKey, config.encKey);
+}
+
+export function decryptKeypair(encrypted: string): Keypair {
+  const secret = decryptPrivateKey(encrypted, config.encKey);
+  return Keypair.fromSecretKey(secret);
+}
